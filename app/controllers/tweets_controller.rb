53e91fe6b9992d5e1a568c7549b970230500ad27
Dino Bilankov <dino.bilankov@gmail.com>
2016-06-22 14:10:50 -0400

    redirect upon successful tweet creation

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,17 +2,22 @@ class TweetsController < ApplicationController
 	def index
 		@tweet = Tweet.new
 	end
+	
 	def create
 		puts "This is a tweet"
 		puts "the params are #{params}"
 		# tweet_params = params.require(:tweet).permit(:message)
 		@tweet = Tweet.new(tweet_params)
 		# @tweet = Tweet.new(params[:tweet]) #Tweet.new(message: "whatever the user typed")
-		@tweet.save
-		render 'index'
+		if @tweet.save
+			#do something
+			redirect_to tweets_path
+		else
+			render 'index'
+		end
 	end
 
-		
+
 
 	def tweet_params
 		params.require(:tweet).permit(:message)
